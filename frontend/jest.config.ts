7db8d4bcfb5463baf3d291a51135b6c9f068854c
jbtestoni <jbn.testoni@gmail.com>
2024-06-17 14:16:22 -0300

    Fix configs

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,12 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    transform: {
-      "^.+\\.[t|j]sx?$": "babel-jest"
-    },
-    testEnvironment: 'jsdom', // This is important for components that interact with the DOM
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1', // Adjust according to your path aliases
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'] // Ignore transformations on node_modules, except where overridden
-  };
-  
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+};
