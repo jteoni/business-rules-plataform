302edbca13de2105979d1403cf726700ef18f2d3
jbtestoni <jbn.testoni@gmail.com>
2024-06-17 14:25:29 -0300

    Add comments and fix functions

--- backend/deps/debts/services.py
+++ backend/deps/debts/services.py
@@ -2,25 +2,51 @@ from uuid import uuid4
 
 class StorageService:
     def __init__(self, bucket_client, bucket_name):
+        """
+        Initializes the StorageService with a bucket client and bucket name.
+
+        :param bucket_client: The client for interacting with the storage bucket.
+        :param bucket_name: The name of the storage bucket.
+        """
         self.bucket_client = bucket_client
         self.bucket_name = bucket_name
 
     def generate_presigned_upload_url(self, file_name, expiration=3600):
-        path = f'{uuid4()}_{file_name}'
+        """
+        Generates a presigned URL for uploading a file to the storage bucket.
+
+        :param file_name: The name of the file to be uploaded.
+        :param expiration: The expiration time (in seconds) for the presigned URL. Default is 3600 seconds (1 hour).
+        :return: Tuple containing the presigned upload URL and the generated file path.
+        """
+        path = f'{uuid4()}_{file_name}'  # Generating a unique path for the file
         upload_url = self.bucket_client.generate_presigned_url(
             'put_object',
             Params={'Bucket': self.bucket_name, 'Key': path},
             ExpiresIn=expiration
-        )
-        return upload_url, path
+        )  # Generating presigned URL for uploading the file
+        return upload_url, path  # Returning the presigned upload URL and the generated file path
 
     def generate_presigned_download_url(self, file_name, expiration=3600):
+        """
+        Generates a presigned URL for downloading a file from the storage bucket.
+
+        :param file_name: The name of the file to be downloaded.
+        :param expiration: The expiration time (in seconds) for the presigned URL. Default is 3600 seconds (1 hour).
+        :return: The presigned download URL.
+        """
         download_url = self.bucket_client.generate_presigned_url(
             'get_object',
             Params={'Bucket': self.bucket_name, 'Key': file_name},
             ExpiresIn=expiration
-        )
-        return download_url
+        )  # Generating presigned URL for downloading the file
+        return download_url  # Returning the presigned download URL
     
     def get_file(self, file_path):
+        """
+        Retrieves a file object from the storage bucket.
+
+        :param file_path: The path of the file to retrieve.
+        :return: The file object.
+        """
         return self.bucket_client.get_object(Bucket=self.bucket_name, Key=file_path)['Body']
--- backend/deps/debts/tests/test_services.py
+++ backend/deps/debts/tests/test_services.py
@@ -2,39 +2,47 @@ from django.test import TestCase
 from unittest.mock import Mock
 from debts.services import StorageService
 
-
+# Test case for StorageService class
 class TestStorageService(TestCase):
     def setUp(self):
+        # Initialize test data
         self.bucket_name = 'test-bucket'
         self.file_name = 'test_file.txt'
         self.expiration = 3600
-        self.bucket_client = Mock()
-        self.storage_service = StorageService(self.bucket_client, self.bucket_name)
+        self.bucket_client = Mock()  # Mocking the bucket client
+        self.storage_service = StorageService(self.bucket_client, self.bucket_name)  # Creating instance of StorageService
 
+    # Test method for generating presigned upload URL
     def test_generate_presigned_upload_url(self):
         expected_upload_url = 'http://presigned.upload.url'
-        self.bucket_client.generate_presigned_url.return_value = expected_upload_url
+        self.bucket_client.generate_presigned_url.return_value = expected_upload_url  # Mocking the return value of generate_presigned_url
 
+        # Calling the method under test
         upload_url, path = self.storage_service.generate_presigned_upload_url(self.file_name, self.expiration)
 
+        # Assertions
         self.bucket_client.generate_presigned_url.assert_called_once_with(
             'put_object',
             Params={'Bucket': self.bucket_name, 'Key': path},
             ExpiresIn=self.expiration
-        )
+        )  # Checking if generate_presigned_url was called correctly
 
-        self.assertEqual(upload_url, expected_upload_url)
-        self.assertTrue(path.endswith(self.file_name))
+        self.assertEqual(upload_url, expected_upload_url)  # Checking if returned upload_url matches expected_upload_url
+        self.assertTrue(path.endswith(self.file_name))  # Checking if the generated path ends with the file name
 
+    # Test method for generating presigned download URL
     def test_generate_presigned_download_url(self):
         expected_download_url = 'http://presigned.download.url'
-        self.bucket_client.generate_presigned_url.return_value = expected_download_url
+        self.bucket_client.generate_presigned_url.return_value = expected_download_url  # Mocking the return value of generate_presigned_url
+
+        # Calling the method under test
         download_url = self.storage_service.generate_presigned_download_url(self.file_name, self.expiration)
 
+        # Assertions
         self.bucket_client.generate_presigned_url.assert_called_once_with(
             'get_object',
             Params={'Bucket': self.bucket_name, 'Key': self.file_name},
             ExpiresIn=self.expiration
-        )
+        )  # Checking if generate_presigned_url was called correctly
 
-        self.assertEqual(download_url, expected_download_url)
+        self.assertEqual(download_url, expected_download_url)  # Checking if returned download_url matches expected_download_url
